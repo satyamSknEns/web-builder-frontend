828b01544c133eb77548214473df224c580edb7b
Satyam Kumar <satyam.kumar@ens.enterprises>
2025-06-05 11:52:19 +0530

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <div className="flex w-full mx-auto px-4">
+      <div className="flex items-center justify-center w-full p-2 text-xl">
+        Web Builder
+      </div>
+    </div>
+  );
+}
